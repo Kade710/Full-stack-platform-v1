636cc79374200af0bebf7f98f6725b38dd15f74b
Kade710 <jonathonkanderson3@gmail.com>
2026-01-05 21:35:37 -0600

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,1 +1,10 @@
+import pg from "pg";
+
+const { Client } = pg;
+
+export function makePgClient(databaseUrl) {
+  if (!databaseUrl) throw new Error("DATABASE_URL is missing");
+  const client = new Client({ connectionString: databaseUrl });
+  return client;
+}
 
