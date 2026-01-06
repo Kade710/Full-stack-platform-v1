fb6c68b5ef1edc9883945e9eec9f3d93621f3c99
Kade710 <jonathonkanderson3@gmail.com>
2026-01-05 21:35:54 -0600

    Update redis.js

--- backend/redis.js
+++ backend/redis.js
@@ -1,1 +1,8 @@
+import { createClient } from "redis";
+
+export function makeRedisClient(redisUrl) {
+  if (!redisUrl) throw new Error("REDIS_URL is missing");
+  const client = createClient({ url: redisUrl });
+  return client;
+}
 
